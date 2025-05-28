a8195e5490a80af1f3c572950dd176f814df81de
wisderfin <wisderfin@yandex.ru>
2025-05-29 00:49:00 +0400

    add CLI mode

---
+++ app/repository/cli/cli.py
@@ -0,0 +1,16 @@
+import typer
+
+
+class CLIRepository:
+    def __init__(self):
+        self.path: str = "app/data/freelancers.py"
+
+    def run(self):
+        while True:
+            question = typer.prompt("Введите вопрос (или 'exit' для выхода)")
+            if question.lower() == "exit":
+                break
+            typer.echo(f"{question}")
+
+
+cli = CLIRepository()
